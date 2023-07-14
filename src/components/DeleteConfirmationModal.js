29fd4ffcbbb0e43bb29daae7e09e92a5ba22c5e8
AebadUQ <aebadulquadir123@gmail.com>
2023-07-14 17:30:37 +0500

    fix

--- src/components/DeleteConfirmationModal.js
+++ src/components/DeleteConfirmationModal.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import MyIcon from '../assets/images/Question.svg'; 
+import { Col, Image,Typography,Row } from 'antd';
 import { StyledModal } from '../common/styledComponents';
+const { Title } = Typography;
+
 const DeleteConfirmationModal = ({ visible, handleConfirmDelete, handleCancelDelete }) => {
   return (
     <StyledModal
     title={
-      <div style={{display:'flex',alignItems:'center',backgroundColor:'#eeeff3'}}>
-        <img src={MyIcon} alt="Image" width={18} height={18}/>
-        <span style={{marginLeft:'4px'}}>Are you sure you want to delete this project?</span>
-      </div>
+      <Row style={{ backgroundColor: '#eeeff3' }} align={"middle"} justify={"start"}>
+      <Col>
+        <Image
+          src={MyIcon}
+          alt="Image"
+          width={18}
+          height={18}
+          preview={false}
+          style={{marginBottom:'5px'}}
+         
+        />
+        <span style={{ marginLeft: '4px'}}>
+          Are you sure you want to delete this project?
+        </span>
+      </Col>
+    </Row>
+      
     }
       visible={visible}
       onCancel={handleCancelDelete}
       onOk={handleConfirmDelete}
-      icon={<img src={MyIcon} alt="Delete Icon" />}
+      icon={<Image src={MyIcon} alt="Delete Icon" />}
       cancelText="No"
       okText="Yes"
 
     >
-      <p style={{marginLeft:'18px',color:'#87888a'}}>
+      <p style={{marginLeft:'20px',color:'#87888a'}}>
       This action can't be undone</p>
     </StyledModal>
   );
