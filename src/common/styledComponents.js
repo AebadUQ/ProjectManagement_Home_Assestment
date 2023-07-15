68d11acf535d0ab21b82ccc5d9b86951804f19d5
AebadUQ <aebadulquadir123@gmail.com>
2023-07-16 01:08:45 +0500

    modal and logo fixes

--- src/common/styledComponents.js
+++ src/common/styledComponents.js
@@ -1,4 +1,4 @@
-import { Modal ,Button,Layout,Typography } from "antd";
+import { Modal ,Button,Layout,Typography,Row,Image } from "antd";
 import styled from "styled-components";
 import { PlusOutlined } from '@ant-design/icons';
 const { Title } = Typography;
@@ -7,6 +7,7 @@ const { Header } = Layout;
 
 export const StyledModal = styled(Modal)`
   .ant-modal-content {
+    margin-top: 4rem;
     background-color: #eeeff3;
     box-shadow: 0 6px 20px rgba(0, 0, 0, 0.3);
     border-radius: 0px;
@@ -58,4 +59,44 @@ export const StyledDiv = styled.div`
 box-shadow: ${({ isDragging }) =>
   isDragging ? '0 0 5px 2px rgba(0, 0, 0, 0.1)' : 'none'};
 ${({ draggableProps }) => draggableProps && draggableProps.style};
+`;
+//delete modal 
+export const ModalButtonYes=styled(Button)`
+
+background-color: rgba(24, 144, 255, 1);
+padding-inline: 1.25rem;
+color: white;
+&:hover {
+      color: white !important;
+    }
+`;
+
+export const ModalButtonNo=styled(Button)`
+
+background-color: transparent;
+padding-inline: 1.25rem;
+&:hover {
+      border:1px solid #d9d9d9;
+    }
+
+`;
+
+export const ModalDesc=styled.p`
+margin-left:26px;
+color: #87888a;
+
+`;
+
+export const ModalTitle=styled.span`
+margin-left:10px;
+
+`;
+
+export const ModalRow=styled(Row)`
+background-color: #eeeff3;
+
+`;
+export const ModalImg=styled(Image)`
+margin-bottom: 5px;
+
 `;
--- src/components/DeleteConfirmationModal.js
+++ src/components/DeleteConfirmationModal.js
@@ -1,41 +1,58 @@
-import React from 'react';
-import MyIcon from '../assets/images/Question.svg'; 
-import { Col, Image,Typography,Row } from 'antd';
-import { StyledModal } from '../common/styledComponents';
-const { Title } = Typography;
+import React from "react";
+import MyIcon from "../assets/images/Question.svg";
+import { Col, Image } from "antd";
+import {
+  StyledModal,
+  ModalButtonYes,
+  ModalButtonNo,
+  ModalDesc,
+  ModalTitle,
+  ModalRow,ModalImg
+} from "../common/styledComponents";
 
-const DeleteConfirmationModal = ({ visible, handleConfirmDelete, handleCancelDelete }) => {
+const DeleteConfirmationModal = ({
+  visible,
+  handleConfirmDelete,
+  handleCancelDelete,
+}) => {
   return (
     <StyledModal
-    title={
-      <Row style={{ backgroundColor: '#eeeff3' }} align={"middle"} justify={"start"}>
-      <Col>
-        <Image
-          src={MyIcon}
-          alt="Image"
-          width={18}
-          height={18}
-          preview={false}
-          style={{marginBottom:'5px'}}
-         
-        />
-        <span style={{ marginLeft: '4px'}}>
-          Are you sure you want to delete this project?
-        </span>
-      </Col>
-    </Row>
-      
-    }
+      closable={false}
+      title={
+        <ModalRow
+          align={"middle"}
+          justify={"start"}
+        >
+          <Col>
+            <ModalImg
+              src={MyIcon}
+              alt="Image"
+              width={22}
+              height={22}
+              preview={false}
+            />
+            <ModalTitle>
+              Are you sure you want to delete this project?
+            </ModalTitle>
+          </Col>
+        </ModalRow>
+      }
       visible={visible}
       onCancel={handleCancelDelete}
       onOk={handleConfirmDelete}
       icon={<Image src={MyIcon} alt="Delete Icon" />}
       cancelText="No"
       okText="Yes"
-
+      footer={[
+        <ModalButtonNo key="cancel" onClick={handleCancelDelete}>
+          No
+        </ModalButtonNo>,
+        <ModalButtonYes key="ok" onClick={handleConfirmDelete}>
+          Yes
+        </ModalButtonYes>,
+      ]}
     >
-      <p style={{marginLeft:'20px',color:'#87888a'}}>
-      This action can't be undone</p>
+      <ModalDesc>This action can't be undone</ModalDesc>
     </StyledModal>
   );
 };
