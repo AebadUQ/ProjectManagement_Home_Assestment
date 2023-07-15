4fc80d446d4b24815a46737ffaa0f5ec9771883f
AebadUQ <aebadulquadir123@gmail.com>
2023-07-16 02:25:00 +0500

    item styling

--- src/common/styledComponents.js
+++ src/common/styledComponents.js
@@ -56,11 +56,7 @@ export const draggableDiv = styled.div`
     color: rgba(66, 66, 66, 1);
   }
 `;
-export const StyledDiv = styled.div`
-box-shadow: ${({ isDragging }) =>
-  isDragging ? '0 0 5px 2px rgba(0, 0, 0, 0.1)' : 'none'};
-${({ draggableProps }) => draggableProps && draggableProps.style};
-`;
+
 //delete modal 
 export const ModalButtonYes=styled(Button)`
 
@@ -140,4 +136,47 @@ margin-bottom: 2rem;
 margin-top: 6px;
 
           
+`;
+
+export const StyledDiv = styled.div`
+box-shadow: ${({ isDragging }) =>
+  isDragging ? '0 0 5px 2px rgba(0, 0, 0, 0.1)' : 'none'};
+${({ draggableProps }) => draggableProps && draggableProps.style};
+`;
+
+//dataitem
+
+export const StyledRowDataItem=styled(Row)`
+padding: 0px;
+margin: 0px;
+margin-bottom: 0px;
+`; 
+
+
+export const DividerTopDataItem=styled(Divider)`
+padding: 0px;
+margin: 0px;
+margin-top: 0px;
+`;
+
+export const StyledInnerRowDataItem = styled.div`
+background-color: white;
+border: ${props => (props.isDeleteHovered ? '0.5px solid black' : '0px')};
+display: flex;
+justify-content: space-between;
+align-items: middle;
+padding-top: 0.75rem;
+padding-bottom: 0.75rem;
+padding-inline: 1.25rem;
+`;
+
+export const DividerBottomDataItem=styled(Divider)`
+padding: 0px;
+margin: 0px;
+margin-bottom: 0px;
+`;
+
+export const EditInput=styled(Input)`
+margin-left: 1rem;
+width: 75%;
 `;
--- src/components/DataItem.js
+++ src/components/DataItem.js
@@ -4,7 +4,15 @@ import beforeHoverImage from "../assets/images/EditIcon.svg";
 import afterHoverImage from "../assets/images/EditIcon_Hover.svg";
 import beforeHoverImageDelete from "../assets/images/DeleteIcon.svg";
 import afterHoverImageDelete from "../assets/images/DeleteIcon_Hover.svg";
-
+import Logo from '../assets/images/logo.png'
+import {
+  StyledRowDataItem,
+  DividerTopDataItem,
+  StyledInnerRowDataItem,
+  StyledRowApp,
+  DividerBottomDataItem,
+  EditInput
+} from "../common/styledComponents";
 const DataItem = ({
   item,
   newData,
@@ -35,92 +43,172 @@ const DataItem = ({
   };
 
   return (
-    <Row justify="center" align="middle" style={{ padding: '0px', margin: '0px', marginBottom: '10px' }}>
-      <Col span={24}>
-      <Divider style={{ padding: "0px", margin: "0px", marginTop: '0px' }} />
+    <StyledRowDataItem justify="center" align="middle" >
+      <Col xs={23} sm={23} md={21} lg={21} xl={21} xxl={21}>
+        <DividerTopDataItem />
 
-        <Row style={{ backgroundColor: "white",border:isDeleteHovered?'0.5px solid black':'0px',paddingInline:'2rem' }} justify="space-between" align="middle">
-
-          <Col
-          span={10}
-            onMouseEnter={handleMouseEnter}
+        <StyledInnerRowDataItem
+       
+          isDeleteHovered={isDeleteHovered}
+          justify="center"
+          align="middle"
+        >
+          <Col span={24} onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            style={{ display: 'flex', alignItems: 'center' }} // Added CSS properties
-          >
-            <Image
-              src={require("../assets/images/logo.png")}
-              preview={false}
-              width={32}
-              height={32}
-              style={{marginRight:'1rem'}}
-            />
+            
+            >
+            <Row  align={"middle"}>
+              <Col xs={14} sm={14} md={12} lg={12} xl={12} xxl={12} >
+                <Row>
+                  <Col span={24}>
+                    <Row align={"middle"}>
+                      <Col
+                        xs={20}
+                        sm={20}
+                        md={20}
+                        lg={20}
+                        xl={20}
+                        xxl={14}
+                      >
+                        <Row align={"middle"}>
+                          <Image
+                            src={Logo}
+                            preview={false}
+                            width={32}
+                            height={32}
+                            style={{ marginRight: "1rem" }}
+                          />
 
-            {editIndex === index ? (
-              <Input
-                value={newData}
-                onChange={(e) => setNewData(e.target.value)}
-                onPressEnter={handleAddData}
-                autoFocus
-                style={{marginLeft:'1rem'}}
-              />
-            ) : (
-              <p style={{ marginInline: '1rem', fontWeight: 'bold' }}>{item.value}</p>
-            )}
-            {editIndex === index ? (
-              <></>
-            ) : (
-              <>
-                {isHovered ? (
-                  <Image
-                    src={afterHoverImage}
-                    alt="After Hover"
-                    style={{ marginRight: "5px", cursor: "pointer" }}
-                    onClick={() => handleEditData(index)}
-                    preview={false}
-
-                  />
-                ) : (
-                  <Image
-                    src={beforeHoverImage}
-                    alt="Before Hover"
-                    style={{ marginRight: "5px", cursor: "pointer" }}
-                    onClick={() => handleEditData(index)}
-                    preview={false}
-
-                  />
-                )}
-              </>
-            )}
+                          {editIndex === index ? (
+                            <EditInput
+                              value={newData}
+                              onChange={(e) => setNewData(e.target.value)}
+                              onPressEnter={handleAddData}
+                              autoFocus
+                            />
+                          ) : (
+                            <p
+                              style={{
+                                marginInline: "1rem",
+                                fontWeight: "bold",
+                              }}
+                            >
+                              {item.value}
+                            </p>
+                          )}
+                        </Row>
+                      </Col>
+                      <Col
+                        xs={4}
+                        sm={4}
+                        md={4}
+                        lg={4}
+                        xl={4}
+                        xxl={10}
+                      >
+                        <Row align={"middle"} justify={"center"}>
+                          <Col>
+                            {editIndex === index ? (
+                              <></>
+                            ) : (
+                              <>
+                                {isHovered ? (
+                                  <Image
+                                    src={afterHoverImage}
+                                    alt="After Hover"
+                                    style={{
+                                      marginRight: "5px",
+                                      cursor: "pointer",
+                                    }}
+                                    onClick={() => handleEditData(index)}
+                                    preview={false}
+                                  />
+                                ) : (
+                                  <Image
+                                    src={beforeHoverImage}
+                                    alt="Before Hover"
+                                    style={{
+                                      marginRight: "5px",
+                                      cursor: "pointer",
+                                    }}
+                                    onClick={() => handleEditData(index)}
+                                    preview={false}
+                                  />
+                                )}
+                              </>
+                            )}
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+              <Col xs={10} sm={10} md={12} lg={12} xl={12} xxl={12} >
+                <Row>
+                  <Col span={24}>
+                    <Row>
+                      <Col
+                        xs={20}
+                        sm={20}
+                        md={20}
+                        lg={12}
+                        xl={12}
+                        xxl={12}
+                      >
+                    <Row justify={"center"}>
+                    {item.createdDate}
+                    </Row>
+                      </Col>
+                      <Col
+                        xs={4}
+                        sm={4}
+                        md={4}
+                        lg={12}
+                        xl={12}
+                        xxl={12}
+                      >
+                        <Row justify={"end"}>
+                          <Col
+                            onMouseEnter={handleDeleteMouseEnter}
+                            onMouseLeave={handleDeleteMouseLeave}
+                          >
+                            {isDeleteHovered ? (
+                              <Image
+                                src={afterHoverImageDelete}
+                                alt="After Hover Delete"
+                                style={{ cursor: "pointer" }}
+                                onClick={() =>
+                                  handleDeleteDataConfirmation(index)
+                                }
+                                preview={false}
+                              />
+                            ) : (
+                              <Image
+                                src={beforeHoverImageDelete}
+                                alt="Before Hover Delete"
+                                style={{ cursor: "pointer" }}
+                                onClick={() =>
+                                  handleDeleteDataConfirmation(index)
+                                }
+                                preview={false}
+                              />
+                            )}
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
           </Col>
-
-          <Col span={10}>
-            <p>{item.createdDate}</p>
-          </Col>
-          <Col onMouseEnter={handleDeleteMouseEnter} onMouseLeave={handleDeleteMouseLeave}>
-            {isDeleteHovered ? (
-              <Image
-                src={afterHoverImageDelete}
-                alt="After Hover Delete"
-                style={{ cursor: "pointer" }}
-                onClick={() => handleDeleteDataConfirmation(index)}
-                preview={false}
-
-              />
-            ) : (
-              <Image
-                src={beforeHoverImageDelete}
-                alt="Before Hover Delete"
-                style={{ cursor: "pointer" }}
-                onClick={() => handleDeleteDataConfirmation(index)}
-                preview={false}
-              />
-            )}
-          </Col>
-        </Row>
-        <Divider style={{ padding: "0px", margin: "0px", marginBottom: '0px' }} />
-
+         
+        </StyledInnerRowDataItem>
+        <DividerBottomDataItem
+        />
       </Col>
-    </Row>
+    </StyledRowDataItem>
   );
 };
 
